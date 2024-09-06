9bc7f785c8cd987fba1d90e04f0fcb589922dc36
Alberto Morgante Medina <alberto.morgante@suse.com>
2024-09-06 10:33:53 +0200

    dpdk file to adapt to atip

--- pktgen_toolbox_generic/scripts/dpdk_devices.sh
+++ pktgen_toolbox_generic/scripts/dpdk_devices.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # return list of all PCI DPDK devices
-output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s)"
+output="$(podman run -it --privileged --rm docker.io/spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s)"
 readarray -t pci_devices <<< "$(echo "$output" | awk '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
 
 for pci in "${pci_devices[@]}"; do
